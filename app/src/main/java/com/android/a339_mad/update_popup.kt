e2f4b8dc3ea69b97fe004d904440347aa1a8616b
Habib ullah <73570477+Habibullah339@users.noreply.github.com>
2022-06-14 18:05:18 +0500

    Update update_popup.kt

--- app/src/main/java/com/android/a339_mad/update_popup.kt
+++ app/src/main/java/com/android/a339_mad/update_popup.kt
@@ -17,50 +17,47 @@ class update_popup : AppCompatActivity() {
 
     }
 
-//    fun updatef(view: View){
-//
-//        val titleu=findViewById<EditText>(com.android.a339_mad.R.id.update_title).toString()
-//        val descu=findViewById<EditText>(com.android.a339_mad.R.id.update_desc).toString()
-//        val updatebtn=findViewById<Button>(com.android.a339_mad.R.id.updatebtn_popup)
-//
-//
-//        updatebtn.setOnClickListener(){
-//            updatedata(titleu, descu)
-//            intent.getStringExtra("title").toString()
-//            intent.getStringExtra("desc").toString()
-//
-//        }
-//
-//
-//    }
-// private  fun  updatedata(titleu: String, descu: String) {
-//
-//
-//     val user = mapOf<String,String>(
-//
-//         "titleu" to titleu,
-//         "descu" to descu,
-//
-//     )
-//
-//
-//     //dbref.child("user").updateChildren(user).addOnSuccessListener
-//
-//     dbref.child("user").child("title").updateChildren(user)
-//         .addOnSuccessListener{
-//
-////         titleu.text.clear()
-////         descu.text.clear()
-////         binding.lastname.text.clear()
-////         binding.age.text.clear()
-//         Toast.makeText(this,"Successfuly Updated",Toast.LENGTH_SHORT).show()
-//
-//
-//     }.addOnFailureListener{
-//
-//         Toast.makeText(this,"Failed to Update",Toast.LENGTH_SHORT).show()
-//
-//     }
+   fun updatef(view: View){
+
+       val titleu=findViewById<EditText>(com.android.a339_mad.R.id.update_title).toString()
+       val descu=findViewById<EditText>(com.android.a339_mad.R.id.update_desc).toString()
+       val updatebtn=findViewById<Button>(com.android.a339_mad.R.id.updatebtn_popup)
+
+
+       updatebtn.setOnClickListener(){
+           updatedata(titleu, descu)
+           intent.getStringExtra("title").toString()
+           intent.getStringExtra("desc").toString()
+
+       }
+
+
+   }
+private  fun  updatedata(titleu: String, descu: String) {
+
+
+    val user = mapOf<String,String>(
+
+        "titleu" to titleu,
+        "descu" to descu,
+
+    )
+
+
+    
+
+    dbref.child("user").child("title").updateChildren(user)
+        .addOnSuccessListener{
+
+
+        Toast.makeText(this,"Successfuly Updated",Toast.LENGTH_SHORT).show()
+
+
+    }.addOnFailureListener{
+
+        Toast.makeText(this,"Failed to Update",Toast.LENGTH_SHORT).show()
+
+    }
 
 
 
