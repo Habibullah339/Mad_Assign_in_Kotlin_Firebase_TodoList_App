ba157f5d46e3ac673fd09cdc1b75c54e5dac2e24
Habib ullah <73570477+Habibullah339@users.noreply.github.com>
2022-06-14 18:04:21 +0500

    Update popup_window.kt

--- app/src/main/java/com/android/a339_mad/popup_window.kt
+++ app/src/main/java/com/android/a339_mad/popup_window.kt
@@ -33,10 +33,7 @@ private lateinit var popup:Button
         title= findViewById(R.id.popup_add_titile)
         desc=findViewById(R.id.popup_desc)
         popup=findViewById(R.id.popup_add_btn)
-//
-//        val title=findViewById<TextView>(R.id.popup_add_titile).toString()
-//       val desc=findViewById<TextView>(R.id.popup_desc).toString()
-//        val popup=findViewById<Button>(R.id.popup_add_btn) as Button
+
         popup.setOnClickListener(){
           val mtitle=title.text.toString()
             val mdesc=desc.text.toString()
@@ -56,59 +53,7 @@ private lateinit var popup:Button
 
     }
 
-//    fun popupadd(view: View){
-//        val title=findViewById<TextView>(R.id.popup_add_titile).toString()
-//        val desc=findViewById<TextView>(R.id.popup_desc).toString()
-//        val popup=findViewById<Button>(R.id.popup_add_btn)
-//
-//
-//        popup.setOnClickListener(){
-//            //Toast.makeText(this@popup_window, "Data Saved !!", Toast.LENGTH_SHORT).show();
-//            saveFirestor(title, desc)
-//
-//
-//
-//        }
-//    }
-//    fun saveFirestor(title: String, desc: String){
-//
-//        val db= FirebaseDatabase.getInstance().reference
-//
-//        db.child("Title").setValue(title).toString()
-//
-//        db.child(" Description").setValue(desc).addOnSuccessListener {
-//            Toast.makeText(this@popup_window, "Data Saved !!", Toast.LENGTH_SHORT).show();
-//                val intent =Intent(this@popup_window, mad_home::class.java)
-//                startActivity(intent)
-//        }
-//            .addOnFailureListener{
-//                Toast.makeText(this@popup_window, "Failed, Data Not Saved !!", Toast.LENGTH_SHORT).show();
-//            }
-//            .toString()
 
-
-
-
-
-
-
-
-
-//        val todo: MutableMap<String, Any> = HashMap()
-//        todo["title"]=title
-//        todo["desc"]=desc
-//        db.collection("user")
-//            .add(todo)
-//            .addOnSuccessListener {
-//                Toast.makeText(this@popup_window, "Data Saved !!", Toast.LENGTH_SHORT).show();
-//                val intent =Intent(this@popup_window, mad_home::class.java)
-//                startActivity(intent)
-//
-//            }
-//            .addOnFailureListener{
-//                Toast.makeText(this@popup_window, "Failed, Data Not Saved !!", Toast.LENGTH_SHORT).show();
-//            }
-   // }
 
 
 }
