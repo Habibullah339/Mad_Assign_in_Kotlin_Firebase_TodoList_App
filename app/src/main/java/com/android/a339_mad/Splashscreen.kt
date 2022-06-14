60a5114948a657725313156a64a5e00dab7d3272
Habib ullah <73570477+Habibullah339@users.noreply.github.com>
2022-06-14 18:01:49 +0500

    Update Splashscreen.kt

--- app/src/main/java/com/android/a339_mad/Splashscreen.kt
+++ app/src/main/java/com/android/a339_mad/Splashscreen.kt
@@ -13,15 +13,13 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        // This is used to hide the status bar and make
-        // the splash screen as a full screen activity.
+       
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
+      
         Handler().postDelayed({
             val intent = Intent(this@SplashScreen, LoginActivity::class.java)
             startActivity(intent)
